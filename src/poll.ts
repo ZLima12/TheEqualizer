5118a2bf25069c9c047686f7c8504a9cd4aae9f2
thebestusernameever <ajay@ajayinkingston.com>
2017-10-12 16:40:25 -0400

    Fix git issues

--- src/poll.ts
+++ src/poll.ts
@@ -206,7 +206,6 @@ namespace Poll
 			return null;
 		}
 
-<<<<<<< HEAD
 		let server: DiscordJS.Guild = message.guild;
 
 		let target: DiscordJS.GuildMember = null;
@@ -259,8 +258,7 @@ namespace Poll
 			return null;
 		}
 
-=======
->>>>>>> a8e3a0994c58d9d9b5f1a276987ac0c9faa21884
+
 		if (message.member.voiceChannel === undefined)
 		{
 			message.reply("You must be in a voice channel to start this vote.");
